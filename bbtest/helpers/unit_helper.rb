dc367c4400ad01c70a2f13fa768f707b0d68e77f
Jan Cajthaml <jan.cajthaml@gmail.com>
2019-05-01 21:11:11 +0200

    Update unit_helper.rb

--- bbtest/helpers/unit_helper.rb
+++ bbtest/helpers/unit_helper.rb
@@ -29,7 +29,6 @@ class UnitHelper
     %x(docker rm temp-container-bondster-bco)
 
     Dir.glob('/opt/artifacts/bondster-bco_*_amd64.deb').each { |f|
-      puts "#{f}"
       FileUtils.mv(f, '/etc/bbtest/packages/bondster-bco.deb')
     }
 
