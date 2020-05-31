a6bfe30a657a61614c8976a0c57b65852f6d5186
Jan Cajthaml <jan.cajthaml@gmail.com>
2020-05-31 09:31:10 +0200

    better interval control

--- services/bondster-bco-import/utils/timeshift.go
+++ services/bondster-bco-import/utils/timeshift.go
@@ -18,7 +18,41 @@ import (
   "time"
 )
 
-func GetMonthsWithin(startDate time.Time, endDate time.Time) []time.Time {
+type TimeRange struct {
+	StartTime time.Time
+	EndTime   time.Time
+}
+
+func (value *TimeRange) String() string {
+	if value == nil {
+		return "<nil>"
+	}
+	return value.StartTime.Format(time.RFC3339) + " - " + value.EndTime.Format(time.RFC3339)
+}
+
+func SliceByMonths(startDate time.Time, endDate time.Time) []TimeRange {
+  dates := make([]TimeRange, 0)
+  current := time.Date(startDate.Year(), startDate.Month(), 1, 0, 0, 0, 0, time.UTC)
+
+  for current.Before(endDate) {
+    date := current.AddDate(0, 1, 0).AddDate(0, 0, -1)
+    dates = append(dates, TimeRange{
+    	StartTime: time.Date(current.Year(), current.Month(), 1, 0, 0, 0, 0, time.UTC),
+    	EndTime: time.Date(date.Year(), date.Month(), date.Day(), 0, 0, 0, 0, time.UTC),
+    })
+    current = date.AddDate(0, 0, 1)
+  }
+	if dates[len(dates)-1].EndTime.After(endDate) {
+		dates[len(dates)-1].EndTime = endDate
+	}
+	if dates[0].StartTime.Before(startDate) {
+		dates[0].StartTime = startDate
+	}
+  return dates
+}
+
+/*
+func GetWeeksWithin(startDate time.Time, endDate time.Time) []time.Time {
   dates := make([]time.Time, 0)
   for ; startDate.Before(endDate); startDate = startDate.AddDate(0, 1, 0) {
     date := startDate.AddDate(0, 1, 0).Add(time.Nanosecond*-1)
@@ -27,3 +61,4 @@ func GetMonthsWithin(startDate time.Time, endDate time.Time) []time.Time {
   }
   return dates
 }
+*/
--- services/bondster-bco-import/utils/timeshift_test.go
+++ services/bondster-bco-import/utils/timeshift_test.go
@@ -7,11 +7,41 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetMonthsWithin(t *testing.T) {
-	start := time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC)
-	end := time.Date(2001, time.January, 1, 0, 0, 0, 0, time.UTC).Add(time.Nanosecond*-1)
+func TestSliceByMonths(t *testing.T) {
+	start := time.Date(2000, time.January, 1, 2, 3, 4, 5, time.UTC)
+	end := time.Date(2000, time.December, 10, 2, 3, 4, 5, time.UTC)
 
-	months := GetMonthsWithin(start, end)
+	timeRanges := SliceByMonths(start, end)
+
+	actual := make([]string, 0)
+	for _, timeRange := range timeRanges {
+		actual = append(actual, timeRange.String())
+	}
+
+	expected := []string{
+		"2000-01-01T02:03:04Z - 2000-01-31T00:00:00Z",
+		"2000-02-01T00:00:00Z - 2000-02-29T00:00:00Z",
+		"2000-03-01T00:00:00Z - 2000-03-31T00:00:00Z",
+		"2000-04-01T00:00:00Z - 2000-04-30T00:00:00Z",
+		"2000-05-01T00:00:00Z - 2000-05-31T00:00:00Z",
+		"2000-06-01T00:00:00Z - 2000-06-30T00:00:00Z",
+		"2000-07-01T00:00:00Z - 2000-07-31T00:00:00Z",
+		"2000-08-01T00:00:00Z - 2000-08-31T00:00:00Z",
+		"2000-09-01T00:00:00Z - 2000-09-30T00:00:00Z",
+		"2000-10-01T00:00:00Z - 2000-10-31T00:00:00Z",
+		"2000-11-01T00:00:00Z - 2000-11-30T00:00:00Z",
+		"2000-12-01T00:00:00Z - 2000-12-10T02:03:04Z",
+	}
+
+	assert.Equal(t, expected, actual)
+}
+
+/*
+func TestGetWeeksWithin(t *testing.T) {
+	start := time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC)
+	end := time.Date(2001, time.February, 1, 0, 0, 0, 0, time.UTC)
+
+	months := GetWeeksWithin(start, end)
 
 	actual := make([]string, 0)
 	for _, month := range months {
@@ -34,4 +64,4 @@ func TestGetMonthsWithin(t *testing.T) {
 	}
 
 	assert.Equal(t, expected, actual)
-}
+}*/
