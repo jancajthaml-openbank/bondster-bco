7259425f214d50aa845f4d5ee396c03d0901d844
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-12-23 11:02:19 +0100

    simplify package docker

--- services/bondster-bco-rest/api/server.go
+++ services/bondster-bco-rest/api/server.go
@@ -83,7 +83,7 @@ func NewServer(
 
 	return &Server{
 		underlying: &http.Server{
-			Addr:         fmt.Sprintf("127.0.0.1:%d", port),
+			Addr:         fmt.Sprintf("0.0.0.0:%d", port),
 			ReadTimeout:  connectionReadTimeout,
 			WriteTimeout: connectionWriteTimeout,
 			Handler:      router,
